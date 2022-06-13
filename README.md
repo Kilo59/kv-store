4092f1253f5ca0c16090744998cd556382136563
Gabriel Gore <gabriel59kg@gmail.com>
2022-06-13 08:46:04 -0400

    initial API

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # kv-store
+
 Simple Key:Value storage and API
+
+## Quick Start
+
+Install python3.10
+[Install `poetry`](https://python-poetry.org/docs/)
+
+```
+brew install poetry
+```
+
+Install dependencies
+
+```
+poetry install
+```
+
+Start api
+
+```
+poetry run invoke api
+```
---
+++ tasks.py
@@ -0,0 +1,37 @@
+import invoke
+
+
+@invoke.task
+def sort(ctx, path=".", check=False):
+    """Sort module imports."""
+    print("  sorting imports ...")
+    args = ["isort", path, "--profile", "black"]
+    if check:
+        args.append("--check-only")
+    ctx.run(" ".join(args))
+
+
+@invoke.task
+def fmt(ctx, path=".", sort_=True, check=False):
+    """Run code formatter."""
+    print("  formatting ...")
+
+    args = ["black", path]
+    if check:
+        args.append("--check")
+    ctx.run(" ".join(args))
+    if sort_:
+        sort(ctx, path, check)
+
+
+@invoke.task
+def lint(ctx, path="kv-store"):
+    ctx.run(f"pylint {path}")
+
+
+@invoke.task
+def api(ctx, dev=True):
+    cmds = ["uvicorn kv_store.api.main:app"]
+    if dev:
+        cmds.append("--reload")
+    ctx.run(" ".join(cmds))
